e61c3c712e8b7072410b395fa4b045077ac05222
z31tothefuture <kabak.espejo@gmail.com>
2021-11-23 20:01:02 -0500

    added Chichaow

---
+++ src/test/java/Steps/Chichaow.java
@@ -0,0 +1,9 @@
+package Steps;
+
+public class Chichaow {
+    public static void main(String[] args) {
+        for (int i = 0; i<5; i++){
+            System.out.println("Looping");
+        }
+    }
+}
